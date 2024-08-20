0369db5f077910d2d6c026eb1b864babe8fcbda0
Sile9t <eyesless1jack@gmail.com>
2024-08-20 20:32:55 +0300

    Added Service library with CompanyService internal sealed class implementing ICompanyService

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be0b1274be4a42c522c03278eba122ac46c5e0ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e401455793c8727b57c0ec99457eb5198c89f97d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LoggerService")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e401455793c8727b57c0ec99457eb5198c89f97d")]
-[assembly: System.Reflection.AssemblyProductAttribute("LoggerService")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LoggerService")]
+[assembly: System.Reflection.AssemblyProductAttribute("Service")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
