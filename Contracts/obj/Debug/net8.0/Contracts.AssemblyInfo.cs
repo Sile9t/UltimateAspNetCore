71129b0b216b64e76aacdbcdbf2d9db6dba61804
Sile9t <eyesless1jack@gmail.com>
2024-08-20 13:38:05 +0300

    Added Repository with defined RepositoryContext with Companies and Employees dbsets

--- Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34872e75c1cc0a3a1fe57ed3b3cbcfbce4e6da3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca4490911a8585ea22e15a3d86475df972f886f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca4490911a8585ea22e15a3d86475df972f886f")]
-[assembly: System.Reflection.AssemblyProductAttribute("Entities")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
+[assembly: System.Reflection.AssemblyProductAttribute("Repository")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
