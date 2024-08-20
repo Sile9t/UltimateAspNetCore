bca4490911a8585ea22e15a3d86475df972f886f
Sile9t <eyesless1jack@gmail.com>
2024-08-20 13:25:50 +0300

    Added Entities library with Company and Employee classes

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34872e75c1cc0a3a1fe57ed3b3cbcfbce4e6da3b")]
-[assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
+[assembly: System.Reflection.AssemblyProductAttribute("Entities")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e38749a2aef80befb643daf125b5682cdfb3489")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34872e75c1cc0a3a1fe57ed3b3cbcfbce4e6da3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
