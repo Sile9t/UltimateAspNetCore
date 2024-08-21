351fcb857a085013d8b277789a8197a7bfcfabf4
Sile9t <eyesless1jack@gmail.com>
2024-08-21 08:06:03 +0300

    Added mapper to services contructors

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,4 +1,5 @@
-﻿using Contracts;
+﻿using AutoMapper;
+using Contracts;
 using Entities;
 using Service.Contracts;
 using Shared.Dtos;
@@ -9,11 +10,12 @@ namespace Service
     {
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
-
-        public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+        private readonly IMapper _mapper;
+        public CompanyService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
         {
             _repository = repository;
             _logger = logger;
+            _mapper = mapper;
         }
 
         public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
