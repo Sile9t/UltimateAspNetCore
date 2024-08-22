3170e546e8f093185e0ff7b6ae04e20636455484
Sile9t <eyesless1jack@gmail.com>
2024-08-22 23:47:16 +0300

    Added RepositoryEmployeeExtensions with age filter and search by name

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -4,7 +4,7 @@ namespace Repository.Extensions
 {
     public static class RepositoryEmployeeExtensions
     {
-        public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee>
+        public static IQueryable<Employee> FilterEmployeesByAge(this IQueryable<Employee>
             employees, uint minAge, uint maxAge) =>
             employees.Where(e => e.Age >= minAge && e.Age <= maxAge);
 
