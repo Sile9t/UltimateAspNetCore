7ecad3bdb5f99248aced7ae64055cda299b0c9e2
Sile9t <eyesless1jack@gmail.com>
2024-08-22 13:02:31 +0300

    Implement employee update and create model to inherit manipulation model

--- Shared/Dtos/EmployeeForCreationDto.cs
+++ Shared/Dtos/EmployeeForCreationDto.cs
@@ -2,16 +2,5 @@
 
 namespace Shared.Dtos
 {
-    public record EmployeeForCreationDto
-    {
-        [Required(ErrorMessage = "Employee name is required field.")]
-        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
-        public string Name { get; init; }
-        [Required(ErrorMessage = "Age is required field.")]
-        [Range(18, int.MaxValue, ErrorMessage = "Age is required and it can't be lower than 18")]
-        public int Age { get; init; }
-        [Required(ErrorMessage = "Position is required field.")]
-        [MaxLength(20, ErrorMessage = "Maximum lenght for the Positio is 20 characters.")]
-        public string Position { get; init; }
-    }
+    public record EmployeeForCreationDto : EmployeeForManipulationDto;
 }
