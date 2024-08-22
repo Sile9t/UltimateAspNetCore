bfa3b2a483e7bc1b4f44033dfe2565fc8c16fe51
Sile9t <eyesless1jack@gmail.com>
2024-08-22 12:59:29 +0300

    Added EmployeeForManipulationDto

--- Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b0ebedebbaf2e12859fe03449bca69cd8a5bdc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+128d63e4f64d278b81b10e7c22768ee27efe9af9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
