4bd4aafd9a3b1b2817a77015f2d30a51b52a3066
Sile9t <eyesless1jack@gmail.com>
2024-08-22 15:05:33 +0300

    Added ValidationFilterAttribute

--- WebApplication1/ActionFilters/ValidationFilterAttribute.cs
+++ WebApplication1/ActionFilters/ValidationFilterAttribute.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.ActionFilters
             var controller = context.RouteData.Values["controller"];
 
             var param = context.ActionArguments
-                .SingleOrDefault(x => x.Value.ToString().Contains("Dto")).Value);
+                .SingleOrDefault(x => x.Value.ToString().Contains("Dto")).Value;
             if (param is null)
             {
                 context.Result = new BadRequestObjectResult($"Object is null. Controller:" +
