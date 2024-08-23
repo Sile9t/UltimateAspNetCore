6007c571bf06a95e66902bf022945a6454644452
Sile9t <eyesless1jack@gmail.com>
2024-08-23 17:17:06 +0300

    Refactored DataShaper from ExpandoObject to Entity

--- Service/DataShaping/DataShaper.cs
+++ Service/DataShaping/DataShaper.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Entities;
 using System.Dynamic;
 using System.Reflection;
 
@@ -13,14 +14,14 @@ namespace Service.DataShaping
             Properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
         }
 
-        public IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString)
+        public IEnumerable<Entity> ShapeData(IEnumerable<T> entities, string fieldsString)
         {
             var requiredProperties = GetRequiredProperties(fieldsString);
 
             return FetchData(entities, requiredProperties);
         }
 
-        public ExpandoObject ShapeData(T entity, string fieldsString)
+        public Entity ShapeData(T entity, string fieldsString)
         {
             var requiredProperties = GetRequiredProperties(fieldsString);
 
@@ -54,10 +55,10 @@ namespace Service.DataShaping
             return requiredProperties;
         }
 
-        private IEnumerable<ExpandoObject> FetchData(IEnumerable<T> entities,
+        private IEnumerable<Entity> FetchData(IEnumerable<T> entities,
             IEnumerable<PropertyInfo> requiredProperties)
         {
-            var shapedData = new List<ExpandoObject>();
+            var shapedData = new List<Entity>();
 
             foreach (var entity in entities)
             {
@@ -68,10 +69,10 @@ namespace Service.DataShaping
             return shapedData;
         }
 
-        private ExpandoObject FetchDataForEntity(T entity, 
+        private Entity FetchDataForEntity(T entity, 
             IEnumerable<PropertyInfo> requiredProperties)
         {
-            var shapedObject = new ExpandoObject();
+            var shapedObject = new Entity();
             foreach (var property in requiredProperties)
             {
                 var objectPropertyValue = property.GetValue(entity);
