8cd02e1e66a7c6b142e3c1e5d390c435360bd160
Sile9t <eyesless1jack@gmail.com>
2024-08-23 17:31:23 +0300

    Added LinkResponse class

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431fb88fd39cc59fddd8034b723cd0eabeb1ee6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6307682b18dc15865aef96fc40168a25d928acc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
