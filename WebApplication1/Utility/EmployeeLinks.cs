5a6b32457d54855d545958fe7b52f7931e6aa945
Sile9t <eyesless1jack@gmail.com>
2024-08-23 21:03:26 +0300

    Added EmployeeLinks

---
+++ WebApplication1/Utility/EmployeeLinks.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using Shared.Dtos;
+
+namespace WebApplication1.Utility
+{
+    public class EmployeeLinks : IEmployeeLinks
+    {
+        private readonly LinkGenerator _linkGenerator;
+        private readonly IDataShaper<EmployeeDto> _dataShaper;
+
+        public EmployeeLinks(LinkGenerator linkGenerator, IDataShaper<EmployeeDto> dataShaper)
+        {
+            _linkGenerator = linkGenerator;
+            _dataShaper = dataShaper;
+        }
+    }
+}
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+048d0c9a90604abd952014ee945dcf44fc898636")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b3a003c8edd2b0e55cfff5a6029c03377a30f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
