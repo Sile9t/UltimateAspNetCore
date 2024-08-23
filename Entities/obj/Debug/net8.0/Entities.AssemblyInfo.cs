ee31b7b413bad52ba694baf09f17917bd29e8cf2
Sile9t <eyesless1jack@gmail.com>
2024-08-23 17:12:22 +0300

    Added Entity class that implements ExpandoObject(DymanicObject, IXmlSerializable, IDictionary<string, object>)

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c82c9b5343fb610aefdab5465cd4bc7b883e96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af50efb761671517cb485e520bd0e92c213c5d13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
