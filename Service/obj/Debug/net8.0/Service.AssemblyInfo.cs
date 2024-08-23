4fc51aaa8445e853f0eef3bc75a536505799eae6
Sile9t <eyesless1jack@gmail.com>
2024-08-23 12:02:28 +0300

    Added DataShaper implementation to Service library

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ee53c1b0d8731ec22690dfc3c7a88b66ad164c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f181e9a9eff4b967962f1504185e061e008fb996")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
