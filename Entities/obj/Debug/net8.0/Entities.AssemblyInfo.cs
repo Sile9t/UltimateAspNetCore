1d24d555e0e075cf10d73e6e66f3255ad9d92e29
Sile9t <eyesless1jack@gmail.com>
2024-08-23 15:50:12 +0300

    Added LinkResourceBase class

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf0d728d9a9a4098f4101ec86b080c5c0620bce9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8143de664eac313a0f1caf9e1475660ecbbbbc9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
