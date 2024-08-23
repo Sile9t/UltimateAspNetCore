be9b8ba733cebbd806437268ef833bca5815ffe6
Sile9t <eyesless1jack@gmail.com>
2024-08-23 12:22:37 +0300

    Modified employee service for data shaping

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -6,6 +6,7 @@ using Service.Contracts;
 using Service.DataShaping;
 using Shared.Dtos;
 using Shared.RequestFeatures;
+using System.Dynamic;
 
 namespace Service
 {
@@ -24,7 +25,7 @@ namespace Service
             _dataShaper = dataShaper;
         }
 
-        public async Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId,
+        public async Task<(IEnumerable<ExpandoObject> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId,
             EmployeeParameters employeeParameters, bool trackChanges)
         {
             if (!employeeParameters.ValidAgeRange)
@@ -34,9 +35,11 @@ namespace Service
 
             var employeesWithMetaData = await _repository.Employee.GetEmployeesAsync(companyId,
                 employeeParameters, trackChanges);
-            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesWithMetaData);
 
-            return (employees: employeesDto, metaData: employeesWithMetaData.MetaData);
+            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesWithMetaData);
+            var shapedData = _dataShaper.ShapeData(employeesDto, employeeParameters.Fields);
+
+            return (employees: shapedData, metaData: employeesWithMetaData.MetaData);
         }
 
         public async Task<EmployeeDto> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges)
