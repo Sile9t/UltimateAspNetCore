cfccfabe7a60de4ac3fc691ca572e75a940a2ebc
Sile9t <eyesless1jack@gmail.com>
2024-08-23 10:26:00 +0300

    Added RepositoryEmployeeExtensions sort method

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b76949a2601a5dc11f4041b32f22ffeabc0751")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51fbdb26a219661f0863dde0f35a64681867f994")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
