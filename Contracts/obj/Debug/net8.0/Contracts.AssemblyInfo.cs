f181e9a9eff4b967962f1504185e061e008fb996
Sile9t <eyesless1jack@gmail.com>
2024-08-23 11:36:18 +0300

    Added ShapeData to RequestParameters and IDataShaper to Contracts

--- Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ee53c1b0d8731ec22690dfc3c7a88b66ad164c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2138e981ddd7ac9c7f1741d60a04fd14e1e37281")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
