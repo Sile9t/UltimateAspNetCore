f3906b53bdecbbc3aea67dd44274dd45aa45f815
Sile9t <eyesless1jack@gmail.com>
2024-08-24 11:45:10 +0300

    Added Head request to EmployeesController

--- CompanyEmployees.Presentation/obj/Debug/net8.0/WebApplication1.Presentation.AssemblyInfo.cs
+++ CompanyEmployees.Presentation/obj/Debug/net8.0/WebApplication1.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a17848fcc2bad558e9506595d4c466af397ba6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba700bda880ca7f14030618814547ecca943232")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
