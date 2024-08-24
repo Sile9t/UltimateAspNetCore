e2fc171328c81467a3b3a19ef7d723d45b8c6a7f
Sile9t <eyesless1jack@gmail.com>
2024-08-24 11:50:00 +0300

    Added RootController

---
+++ CompanyEmployees.Presentation/Controllers/RootController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace WebApplication1.Presentation.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        public RootController(LinkGenerator linkGenerator) => _linkGenerator = linkGenerator;
+
+    }
+}
--- CompanyEmployees.Presentation/obj/Debug/net8.0/WebApplication1.Presentation.AssemblyInfo.cs
+++ CompanyEmployees.Presentation/obj/Debug/net8.0/WebApplication1.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba700bda880ca7f14030618814547ecca943232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3906b53bdecbbc3aea67dd44274dd45aa45f815")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
