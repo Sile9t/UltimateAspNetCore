d0e7f646b9166407a2306be73256b721d065d2bd
Sile9t <eyesless1jack@gmail.com>
2024-08-24 16:24:12 +0300

    Added CompaniesV2Controller

--- CompanyEmployees.Presentation/obj/Debug/net8.0/WebApplication1.Presentation.AssemblyInfo.cs
+++ CompanyEmployees.Presentation/obj/Debug/net8.0/WebApplication1.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21136d2707d775c855be816a01208131bb5b16de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b7439bf6460847d49c63e9cb6c4f64978b54d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
