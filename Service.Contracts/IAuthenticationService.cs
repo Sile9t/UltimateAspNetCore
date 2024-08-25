563565a2529aad3209d5858d5ebfc1886c6581e1
Sile9t <eyesless1jack@gmail.com>
2024-08-25 10:32:09 +0300

    Added IAuthenticationService

---
+++ Service.Contracts/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Shared.Dtos;
+
+namespace Service.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto);
+    }
+}
