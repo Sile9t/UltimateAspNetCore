7cda25184c93077bb2117b1f52a06d5142e68248
Sile9t <eyesless1jack@gmail.com>
2024-08-25 10:20:01 +0300

    Adde AuthenticationController

---
+++ WebApplication1/Migrations/20240825071401_AddedRolesDB.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRolesDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "d6158192-073b-4a0c-b537-11486a3c720a", null, "Administrator", "ADMINISTRATOR" },
+                    { "efd08b5e-8081-40bf-8098-f210d28a9278", null, "Manager", "MANAGER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d6158192-073b-4a0c-b537-11486a3c720a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "efd08b5e-8081-40bf-8098-f210d28a9278");
+        }
+    }
+}
