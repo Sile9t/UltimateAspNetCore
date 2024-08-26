0be2e08096450b6b64d73eca4e232e7304613bba
Sile9t <eyesless1jack@gmail.com>
2024-08-26 23:02:51 +0300

    Added warning suppress

--- CompanyEmployees.Presentation/obj/Debug/net8.0/WebApplication1.Presentation.AssemblyInfo.cs
+++ CompanyEmployees.Presentation/obj/Debug/net8.0/WebApplication1.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8071d1843dd726ac083b42b91961ff33b987c558")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cda1c974a1460e7f868a926a6845fd13213d37f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
