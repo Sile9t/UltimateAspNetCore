d957a8153225ee4d3200f65e188f5713d973e365
Sile9t <eyesless1jack@gmail.com>
2024-08-27 10:42:15 +0300

    Added ApiBaseResponseExtensions.GetResult method

--- CompanyEmployees.Presentation/obj/Debug/net8.0/WebApplication1.Presentation.AssemblyInfo.cs
+++ CompanyEmployees.Presentation/obj/Debug/net8.0/WebApplication1.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0145b6d549f5e60d8b9a2f3e39dc3c226a63b4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428a29a86bbddc2b2edf3c8133ef0dcdbe18d889")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
