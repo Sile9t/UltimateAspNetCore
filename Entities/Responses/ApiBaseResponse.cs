8dce46de7d0cfef03c3a6994b41522f6bbecdffd
Sile9t <eyesless1jack@gmail.com>
2024-08-27 09:38:21 +0300

    Added ApiBaseResponse abstract class

---
+++ Entities/Responses/ApiBaseResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Responses
+{
+    public abstract class ApiBaseResponse
+    {
+        public bool Success { get; set; }
+
+        protected ApiBaseResponse(bool success) => Success = success;
+    }
+}
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0b95652606d7713f5c20f41b0e6a9439b9022e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec5360ae03251ca3b87a7b4bff8c26ceb3d37e38")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
