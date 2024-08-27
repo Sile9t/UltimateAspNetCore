fe5c6cbd51265d22b395442b0b4f91ebb1b8bae4
Sile9t <eyesless1jack@gmail.com>
2024-08-27 09:49:56 +0300

    Added ApiBadRequestResponse abstract class

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f2c595981638a1700575a332eabd5eefab7b36f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7af895bbcab85e73f6119a0f8e9ce3e025669c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
